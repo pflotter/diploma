8fcd2ff04ac5624f1ca505339dc1ac410210cee7
Vadim <playfulotterspb@gmail.com>
2022-05-24 23:37:49 +0300

    UPD: Налажен вывод в функцию построения второго окна

--- criteria/shapiro_wilk/test.py
+++ criteria/shapiro_wilk/test.py
@@ -1,15 +1,23 @@
 import numpy as np
 from scipy.stats import shapiro
 
-
-def check(data: np.array):
+def check(data: np.array) -> np.array:
     stat, p = shapiro(data)
-    print('Статистика Шапиро-Уилка = %.3f, p=%.3f' % (stat, p))
+    ans = ""
+    tmp = []
+    print('Статистика Шапиро-Уилка = %.10f, p=%.10f' % (stat, p))
 
     # альфа-уровень принятия гиипотезы
     alpha = 0.05
 
     if p > alpha:
+        ans = "нормальное распределение"
         print("данные имеют нормальное распределение")
     else:
+        ans = "ненормальное распределение"
         print('данные распределены не по нормальному закону')
+
+    tmp.append([stat, 'Статистика Шапиро-Уилка'])
+    tmp.append([p, 'P-значение ({})'.format(ans)])
+
+    return tmp
