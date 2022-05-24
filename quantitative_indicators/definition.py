b86c2fb06b4191e8c8928f1b69da1ce9cf602974
Vadim <playfulotterspb@gmail.com>
2022-05-24 22:11:27 +0300

    UPD: Удалена некорректная функция отклонения по оси

--- quantitative_indicators/definition.py
+++ quantitative_indicators/definition.py
@@ -3,41 +3,38 @@ from scipy import stats
 
 
 # мат. ожидание по выборке data
-def get_mean(data: np.array) -> float:
-    return round(np.mean(data), 3)
+def get_mean(data: np.array) -> np.array:
+    return [round(np.mean(data), 3), 'Мат. ожидание: ']
 
 
 # среднеквадратичное отклонение по выборке data
-def get_standard_deviation(data: np.array) -> float:
-    return round(np.std(data), 3)
+def get_standard_deviation(data: np.array) -> np.array:
+    return [round(np.std(data), 3), 'Среднеквадратичное отклонение: ']
 
 
 # медиана по выборке data
-def get_median(data: np.array) -> float:
-    return np.median(data)
+def get_median(data: np.array) -> np.array:
+    return [np.median(data), 'Медиана: ']
 
 
 # мода по выборке data
-def get_mode(data: np.array) -> float:
-    return float(stats.mode(data)[0])
+def get_mode(data: np.array) -> np.array:
+    return [float(stats.mode(data)[0]), 'Мода: ']
 
 
 # диапазон значений по выборке data
-def get_range(data: np.array) -> float:
-    return max(data) - min(data)
+def get_range(data: np.array) -> np.array:
+    return [max(data) - min(data), 'Диапазон значений: ']
 
 
 # межквартильный диапазон по выборке data:
-def get_interquartile_range(data: np.array) -> float:
+def get_interquartile_range(data: np.array) -> np.array:
     tmp = sorted(data)
     quartile_1 = np.median(tmp[:len(tmp) // 2])
     quartile_3 = np.median(tmp[(len(tmp) + 1) // 2:])
-    return quartile_3 - quartile_1
+    return [quartile_3 - quartile_1, 'Межквартильный диапазон: ']
 
 
-# стандартное отклонение по выборке data:
-def get_variance(data: np.array) -> float:
-    return np.var(data)
 
 
 
