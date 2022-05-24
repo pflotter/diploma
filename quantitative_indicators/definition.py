f46366a0f766b25838d4e9b42b0747d5b6dfc7d7
Vadim <playfulotterspb@gmail.com>
2022-05-24 17:44:51 +0300

    UPD: UI

---
+++ quantitative_indicators/definition.py
@@ -0,0 +1,23 @@
+### Normal distribution
+
+# Independent groups
+def student_t_criterion(title: str='') -> None:
+    return title
+
+# Dependent groups
+
+        # Парный критерий стьюдента
+    #def student_paired_criterion
+
+################################################################################
+### Not normal distribution
+
+# Independent groups
+    #def mann_whitney_criterion
+
+# Dependent groups
+
+    #def kolmogorov_smirnov_criterion
+
+
+
