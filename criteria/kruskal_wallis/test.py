fef467e6be6e67c78fdcdcfa2beee796b013d956
thank4this <chernyshevuser@gmail.com>
2022-05-26 14:32:58 +0300

    крускал-уоллис, неограниченное число параметров

--- criteria/kruskal_wallis/test.py
+++ criteria/kruskal_wallis/test.py
@@ -4,7 +4,7 @@ from scipy.stats import kruskal
 
 def go(all_data: np.array, alpha: float = 0.05) -> np.array:
 
-    stat, p = kruskal(all_data[0], all_data[1], all_data[2])
+    stat, p = kruskal(*all_data)
     print('Statistics=%.3f, p=%.3f' % (stat, p))
 
     if p > alpha:
